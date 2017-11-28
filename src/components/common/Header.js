81a347d0e4131b90a170b8aad64a8f13be39d338
Patryk Przekwas <pkprzekwas@gmail.com>
2017-11-28 23:48:15 +0100

    Added session view

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -4,9 +4,11 @@ import { Link, IndexLink } from 'react-router';
 const Header = () => {
   return (
     <nav>
-      <IndexLink to="/" activeClassName="active">Home</IndexLink>
+      <IndexLink to="/" activeClassName="active">Główna</IndexLink>
       {" | "}
-      <Link to="/about" activeClassName="active">About</Link>
+      <Link to="/session" activeClassName="active">Sesja</Link>
+      {" | "}
+      <Link to="/about" activeClassName="active">Info</Link>
     </nav>
   );
 };
--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,12 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/App';
 import HomePage from './components/home/HomePage';
 import AboutPage from './components/about/AboutPage';
+import SessionPage from './components/sessions/SessionPage';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage} />
+    <Route path="session" component={SessionPage} />
     <Route path="about" component={AboutPage} />
   </Route>
 );
